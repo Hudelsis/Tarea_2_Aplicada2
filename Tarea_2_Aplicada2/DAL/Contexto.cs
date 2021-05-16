d7c18d660bd8f9fc42f09ebc8b7d2ea956a5ecaf
Hudelsis Antigua Mendez <antiguamendez27@gmail.com>
2021-05-16 05:40:01 +0200

    Agregar archivos de proyecto.

---
+++ Tarea_2_Aplicada2/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tarea_2_Aplicada2.Models;
+
+namespace Tarea_2_Aplicada2.DAL
+{
+          public class Contexto : DbContext
+        {
+          public DbSet<Personas> Personas { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite(@"Data Source = Data\Personas.db");
+            }
+        }
+ }
+
